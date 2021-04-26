beba956b3516bff74f441121240f63164b1fab49
Gregory Moskalev <gregory.moskalev@gmail.com>
2021-04-26 14:13:24 +0300

    add withRouter

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,13 @@
 const Post = ({id}) => <h1>Post Page # {id}</h1>;
 
 Post.getInitialProps = async ({query}) => {
-  console.log(query);
   return query;
 };
 
 export default Post;
+
+// import {withRouter} from "next/router";
+
+// const Post = (props) => <h1>Post Page # {props.router.query.id}</h1>;
+
+// export default withRouter(Post);
